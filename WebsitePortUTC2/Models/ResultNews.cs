ebea6e1dc111cf77c062ee7e6b755b27e3235eea
Trinh Le <khanhtrinh.phuyen@gmail.com>
2023-12-14 10:08:22 +0700

    s

--- WebsitePortUTC2/Models/ResultNews.cs
+++ WebsitePortUTC2/Models/ResultNews.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string NameSlug { get; set; }
-        public int SchoolId { get; set; }
+        public int? SchoolId { get; set; }
         public int? ParentId { get; set; }
-        public string Remark { get; set; }
-        public int Sort { get; set; }
+        public string? Remark { get; set; }
+        public int? Sort { get; set; }
         public int Status { get; set; }
         public DateTime? CreatedAt { get; set; }
         public int? CreatedBy { get; set; }
@@ -20,32 +20,32 @@
     public class ImageObj
     {
         public int Id { get; set; }
-        public string RelativeUrl { get; set; }
-        public string SmallUrl { get; set; }
-        public string MediumUrl { get; set; }
+        public string? RelativeUrl { get; set; }
+        public string? SmallUrl { get; set; }
+        public string? MediumUrl { get; set; }
     }
 
     public class NewsModel
     {
         public int Id { get; set; }
-        public int SchoolId { get; set; }
-        public int NewsCategoryId { get; set; }
-        public string Name { get; set; }
-        public string NameSlug { get; set; }
-        public string Description { get; set; }
+        public int? SchoolId { get; set; }
+        public int? NewsCategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? NameSlug { get; set; }
+        public string? Description { get; set; }
         public int? ImageId { get; set; }
-        public string Detail { get; set; }
-        public bool IsHot { get; set; }
-        public int ViewNumber { get; set; }
+        public string? Detail { get; set; }
+        public bool? IsHot { get; set; }
+        public int? ViewNumber { get; set; }
         public DateTime PublishedAt { get; set; }
-        public string MetaKeywords { get; set; }
-        public string MetaDescription { get; set; }
-        public string MetaTitle { get; set; }
-        public string MetaUrl { get; set; }
-        public string MetaImagePreview { get; set; }
+        public string? MetaKeywords { get; set; }
+        public string? MetaDescription { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaUrl { get; set; }
+        public string? MetaImagePreview { get; set; }
         public ImageObj ImageObj { get; set; }
         public NewsCategoryObj NewsCategoryObj { get; set; }
-        public int Status { get; set; }
+        public int? Status { get; set; }
         public DateTime? CreatedAt { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
@@ -55,18 +55,18 @@
 
     public class ApiResponse
     {
-        public int Result { get; set; }
-        public long Time { get; set; }
-        public string DataDescription { get; set; }
+        public int? Result { get; set; }
+        public long? Time { get; set; }
+        public string? DataDescription { get; set; }
         public List<NewsModel> Data { get; set; }
-        public int Data2nd { get; set; }
+        public int? Data2nd { get; set; }
         public ErrorObj Error { get; set; }
     }
 
     public class ErrorObj
     {
         public int Code { get; set; }
-        public string Message { get; set; }
+        public string? Message { get; set; }
     }
     public class ResultNews
     {
